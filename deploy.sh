82abc6b7ae8415b921c65e240fd10141abf01b96
Philip Potter <philip.g.potter@gmail.com>
2017-05-30 09:59:55 +0100

    deploy to main site, instead of spike prefix site

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,14 @@
 
 set -eu
 
-PREFIX=s3://wind.cards/offline
+PREFIX=s3://wind.cards
 
 # html
 aws s3 cp assets/bassoon.html ${PREFIX}/bassoon --content-type 'text/html; charset=utf-8' --cache-control 'max-age=300'
 
 # css & other assets
+# we deliberately avoid the `--delete` flag,
+# to preserve older fingerprinted assets
 aws s3 sync assets $PREFIX --exclude '*' --include '*.css' --cache-control 'max-age=300' --content-type 'text/css; charset=utf-8'
 aws s3 sync assets $PREFIX --exclude '*' --include '*.svg' --cache-control 'max-age=300' --content-type 'image/svg+xml'
 aws s3 sync assets $PREFIX --exclude '*' --include '*.js' --cache-control 'max-age=300' --content-type 'application/javascript'
