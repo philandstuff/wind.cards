b1aedb5841ab4ed73e037e4e34314b208f3e6ac6
Philip Potter <philip.g.potter@gmail.com>
2017-06-04 14:59:35 +0100

    this... didn't actually work

--- bassoon/clef.js
+++ bassoon/clef.js
@@ -1,4 +1,4 @@
-import { Accidental, Formatter, Renderer, Stave, StaveNote, Voice } from 'vexflow';
+import { Flow } from 'vexflow';
 
 function clefFor(midiNum) {
   if (midiNum < 57) {
@@ -25,10 +25,10 @@ function midi2VF(midiNum) {
 
 function noteFor(clef, midiNum) {
   const vfNote = midi2VF(midiNum);
-  const note = new StaveNote({ clef, keys: [vfNote.note], duration: 'q' });
+  const note = new Flow.StaveNote({ clef, keys: [vfNote.note], duration: 'q' });
 
   if (vfNote.accidental) {
-    note.addAccidental(0, new Accidental(vfNote.accidental));
+    note.addAccidental(0, new Flow.Accidental(vfNote.accidental));
   }
   return note;
 }
@@ -36,12 +36,12 @@ function noteFor(clef, midiNum) {
 function notesFor(clef, midiNum, midiNums) {
   const vfNotes = midiNums.map(num => midi2VF(num));
   const degrees = vfNotes.map(vfNote => vfNote.note);
-  const note = new StaveNote({ clef, keys: degrees, duration: 'q' });
+  const note = new Flow.StaveNote({ clef, keys: degrees, duration: 'q' });
 
   for (let i = 0; i < midiNums.length; i += 1) {
     const vfNote = vfNotes[i];
     if (vfNote.accidental) {
-      note.addAccidental(i, new Accidental(vfNote.accidental));
+      note.addAccidental(i, new Flow.Accidental(vfNote.accidental));
     }
     if (i !== midiNums.indexOf(midiNum)) {
       note.setKeyStyle(i, { fillStyle: 'gray' });
@@ -58,7 +58,7 @@ export default function drawNotes(lower, upper, uppers) {
     element.removeChild(element.firstChild);
   }
 
-  const renderer = new Renderer(element, Renderer.Backends.SVG);
+  const renderer = new Flow.Renderer(element, Flow.Renderer.Backends.SVG);
   const width = 200;
   renderer.resize(width, 140);
   const context = renderer.getContext();
@@ -66,12 +66,12 @@ export default function drawNotes(lower, upper, uppers) {
   const clef = clefFor(lower);
 
   // stave is width-1 to allow final barline to be visible
-  const stave = new Stave(0, 0, width - 1);
+  const stave = new Flow.Stave(0, 0, width - 1);
   stave.addClef(clef).setContext(context).draw();
 
-  const voice = new Voice({ num_beats: 2, beat_value: 4 });
+  const voice = new Flow.Voice({ num_beats: 2, beat_value: 4 });
   voice.addTickables([noteFor(clef, lower), notesFor(clef, upper, uppers)]);
 
-  new Formatter().joinVoices([voice]).format([voice], width);
+  new Flow.Formatter().joinVoices([voice]).format([voice], width);
   voice.draw(context, stave);
 }
