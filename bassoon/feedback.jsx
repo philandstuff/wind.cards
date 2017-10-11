3ab52650b984a03c655888e863834957bdcc570b
Philip Potter <philip.g.potter@gmail.com>
2017-10-11 21:46:20 +0100

    extract FeedbackModal into separate file

---
+++ bassoon/feedback.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Modal from 'react-modal';
+
+
+export default class FeedbackModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.sendFeedback = this.sendFeedback.bind(this);
+  }
+
+  resetForm() {
+    this.feedbackTextarea.value = '';
+  }
+
+  sendFeedback() {
+    const toSend = {
+      name: this.feedbackTextarea.value,
+      data: this.props.state,
+    };
+    fetch('https://feedback.wind.cards', {
+      method: 'post',
+      mode: 'cors',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(toSend),
+    })
+      .then(response => {
+        this.resetForm();
+        this.props.onSuccess();
+      })
+      .catch(error => console.log('Request failed', error));
+  }
+
+  render() {
+    return (
+      <Modal
+        isOpen={this.props.isOpen}
+        onRequestClose={this.props.onRequestClose}
+        overlayClassName="feedback-modal-overlay"
+        className="feedback-modal"
+      >
+        <svg
+          viewbox="0 0 40 40"
+          style={{
+            position: 'absolute',
+            width: 40,
+            right: 20,
+            top: 20,
+            cursor: 'pointer',
+          }}
+          onClick={this.props.onRequestClose}
+        >
+          <path
+            style={{
+              stroke: 'black',
+              fill: 'transparent',
+              strokeLinecap: 'round',
+              strokeWidth: 5,
+            }}
+            d="M 10,10 L 30,30 M 30,10 L 10,30"
+          />
+        </svg>
+        <h2>Report a problem</h2>
+        <form>
+          <textarea id="feedback" ref={c => { this.feedbackTextarea = c; }} />
+          <button type="button" onClick={this.sendFeedback}>Submit</button>
+        </form>
+      </Modal>
+    );
+  }
+}
+
+FeedbackModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onSuccess: PropTypes.func.isRequired,
+  onRequestClose: PropTypes.func.isRequired,
+  state: PropTypes.object.isRequired,
+};
--- bassoon/view.jsx
+++ bassoon/view.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-import Modal from 'react-modal';
 import { I, maybe } from 'sanctuary';
 import { lowerNotes, initialState, setLower, setUpper, prevFingering, nextFingering, fingering, upperNoteChoices, fingeringsAvailable } from './model';
 import Fingering from './fingering';
 import VexFlow from './stave';
 import TwitterImage from '../twitter.svg';
+import FeedbackModal from './feedback';
 
 
 function FingeringNav(props) {
@@ -50,83 +49,6 @@ function NoteNav(props) {
     </div>
   );
 }
-
-
-class FeedbackModal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.sendFeedback = this.sendFeedback.bind(this);
-  }
-
-  resetForm() {
-    this.feedbackTextarea.value = '';
-  }
-
-  sendFeedback() {
-    const toSend = {
-      name: this.feedbackTextarea.value,
-      data: this.props.state,
-    };
-    fetch('https://feedback.wind.cards', {
-      method: 'post',
-      mode: 'cors',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(toSend),
-    })
-      .then(response => {
-        this.resetForm();
-        this.props.onSuccess();
-      })
-      .catch(error => console.log('Request failed', error));
-  }
-
-  render() {
-    return (
-      <Modal
-        isOpen={this.props.isOpen}
-        onRequestClose={this.props.onRequestClose}
-        overlayClassName="feedback-modal-overlay"
-        className="feedback-modal"
-      >
-        <svg
-          viewbox="0 0 40 40"
-          style={{
-            position: 'absolute',
-            width: 40,
-            right: 20,
-            top: 20,
-            cursor: 'pointer',
-          }}
-          onClick={this.props.onRequestClose}
-        >
-          <path
-            style={{
-              stroke: 'black',
-              fill: 'transparent',
-              strokeLinecap: 'round',
-              strokeWidth: 5,
-            }}
-            d="M 10,10 L 30,30 M 30,10 L 10,30"
-          />
-        </svg>
-        <h2>Report a problem</h2>
-        <form>
-          <textarea id="feedback" ref={c => { this.feedbackTextarea = c; }} />
-          <button type="button" onClick={this.sendFeedback}>Submit</button>
-        </form>
-      </Modal>
-    );
-  }
-}
-
-FeedbackModal.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
-  onSuccess: PropTypes.func.isRequired,
-  onRequestClose: PropTypes.func.isRequired,
-  state: PropTypes.object.isRequired,
-};
 
 
 class Site extends React.Component {
